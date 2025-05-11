b6899797dffbb0f557b61b7cd5e0b399035bae49
Dana Goosen <klymentovska@gmail.com>
2025-05-11 13:19:29 +0200

    Create background

--- code/main.py
+++ code/main.py
@@ -1,5 +1,7 @@
 import pygame, sys, time
+from os.path import join
 from settings import *
+from sprites import BG
 
 class Game:
     def __init__(self):
@@ -9,6 +11,17 @@ class Game:
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption('Flappy Bird')
         self.clock = pygame.time.Clock()
+        
+        # sprite groups
+        self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
+        
+        # scale factor
+        bg_height = pygame.image.load(join('graphics', 'environment', 'background.png')).get_height()
+        self.scale_factor = WINDOW_HEIGHT / bg_height
+        
+        # sprite setup
+        BG(self.all_sprites, self.scale_factor)
         
     def run(self):
         last_time = time.time()
@@ -23,8 +36,12 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                    
-            # game logic
+
+            # game logic 
+            self.display_surface.fill('black')
+            self.all_sprites.update(dt)
+            self.all_sprites.draw(self.display_surface)
+            
             pygame.display.update()
             self.clock.tick(FRAMERATE)
 
