d0db07a8e27a7019f05b335a0bc592433b51db56
Dana Goosen <klymentovska@gmail.com>
2025-05-14 15:05:21 +0200

    Refactor Ground

--- code/sprites/ground.py
+++ code/sprites/ground.py
@@ -4,23 +4,32 @@ from os.path import join
 class Ground(pygame.sprite.Sprite):
     def __init__(self, groups, scale_factor):
         super().__init__(groups)
-        self.sprite_type = 'ground'
-        
-        # image
-        ground_surf = pygame.image.load(join('graphics', 'environment', 'ground.png')).convert_alpha()
-        self.image = pygame.transform.scale(ground_surf, pygame.math.Vector2(ground_surf.get_size()) * scale_factor)
-        
-        # position
-        self.rect = self.image.get_rect(bottomleft=(0,WINDOW_HEIGHT))
-        self.pos = pygame.math.Vector2(self.rect.topleft)
-        
-        # masks
-        self.mask = pygame.mask.from_surface(self.image)
+        self.__set_image(scale_factor)
+        self.__set_rect()
+        self.__set_pos()
+        self.__set_mask()
         
     def update(self, dt):
-        self.pos.x -= 360 * dt
-        
+        self.__move_left(dt)
+            
+    def __move_left(self, dt):
+        self.pos.x -= GROUND_SPEED * dt
+        self.__loop_ground()
+        self.rect.x = round(self.pos.x)
+            
+    def __loop_ground(self):
         if self.rect.centerx <= 0:
             self.pos.x = 0
-            
-        self.rect.x = round(self.pos.x)
+    
+    def __set_image(self, scale_factor):
+        surf = pygame.image.load(join('graphics', 'environment', 'ground.png')).convert_alpha()
+        self.image = pygame.transform.scale(surf, pygame.math.Vector2(surf.get_size()) * scale_factor)
+    
+    def __set_rect(self):
+        self.rect = self.image.get_rect(bottomleft=(0,WINDOW_HEIGHT))
+    
+    def __set_pos(self):
+        self.pos = pygame.math.Vector2(self.rect.topleft)
+        
+    def __set_mask(self):
+        self.mask = pygame.mask.from_surface(self.image)
